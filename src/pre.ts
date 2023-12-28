3219c12dd24a814f055131ad787248dc60bf861f
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-12-27 19:21:31 -0700

    Clean up input

--- src/pre.ts
+++ src/pre.ts
@@ -3,10 +3,25 @@ import { exportVariable, getInput } from '@actions/core';
 import { join } from 'node:path';
 
 export class PreSetup {
+	protected cleanModelName: string;
+
+	constructor() {
+		let parts1 = getInput('model', { required: true }).split('/');
+
+		// Remove "TheBloke" from the array if it exists
+		parts1 = parts1.filter((part) => part.toLowerCase() !== 'TheBloke'.toLowerCase());
+
+		let parts2 = parts1.join('/').split('-');
+
+		// Remove "GGUF" from the array if it exists
+		parts2 = parts2.filter((part) => part.toLowerCase() !== 'GGUF'.toLowerCase());
+
+		this.cleanModelName = parts2.join('-');
+	}
+
 	public async main() {
 		if (isFeatureAvailable()) {
-			console.log(getInput('model', { required: true }), getInput('quantMethod', { required: true }), getInput('modelDir', { required: true }));
-			console.log(join(getInput('modelDir', { required: true }), getInput('model', { required: true })));
+			console.log(join(getInput('modelDir', { required: true }), this.cleanModelName, getInput('quantMethod', { required: true }), '.gguf'));
 		} else {
 			// TODO: Download
 		}
