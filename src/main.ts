2a2f2a23956f1e30fe33a6f1f940b9b889e9395f
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2023-12-27 18:57:17 -0700

    Run pre in main if it hasn't before

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,7 @@
+import { PreSetup } from './pre.js';
+
+if (process.env['COLDFUSION_CORE_PRE_EXECUTED'] !== `${true}`) {
+	await new PreSetup().main();
+}
+
 console.log('Hello world');
--- src/pre.ts
+++ src/pre.ts
@@ -1,10 +1,18 @@
 import { isFeatureAvailable } from '@actions/cache';
-import { getInput } from '@actions/core';
+import { exportVariable, getInput } from '@actions/core';
 import { join } from 'node:path';
 
-if (isFeatureAvailable()) {
-	console.log(getInput('model', { required: true }), getInput('quantMethod', { required: true }), getInput('modelDir', { required: true }));
-	console.log(join(getInput('modelDir', { required: true }), getInput('model', { required: true })));
-} else {
-	// TODO: Download
+export class PreSetup {
+	public async main() {
+		if (isFeatureAvailable()) {
+			console.log(getInput('model', { required: true }), getInput('quantMethod', { required: true }), getInput('modelDir', { required: true }));
+			console.log(join(getInput('modelDir', { required: true }), getInput('model', { required: true })));
+		} else {
+			// TODO: Download
+		}
+
+		exportVariable('COLDFUSION_CORE_PRE_EXECUTED', `${true}`);
+	}
 }
+
+await new PreSetup().main();
