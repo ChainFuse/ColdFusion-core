a4368c0ebc0b8848f3cc4f5f160b921b7b5923d0
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-08-13 01:53:53 -0700

    Not used

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,5 @@
-import { info } from '@actions/core';
 import { cpus, platform } from 'node:os';
 import { arch } from 'node:process';
-import { graphics } from 'systeminformation';
 import { BaseCore } from './base.js';
 import { PreCore } from './pre.js';
 
@@ -18,22 +16,12 @@ export class MainCore extends BaseCore {
 		return cpus()[0]!.model.includes('Apple') && arch === 'arm64';
 	}
 
-	private static get vramAmount() {
-		return new Promise<number>((resolve, reject) => {
-			graphics()
-				.then((data) => resolve(data.controllers.reduce((total, controller) => total + (controller.vram || 0), 0)))
-				.catch(reject);
-		});
-	}
-
 	private static async pre() {
 		if (platform() === 'darwin') {
 			if (!this.isMetalSupported) {
 				throw new Error('Metal is not supported on this system', { cause: cpus()[0]!.model });
 			}
 		}
-
-		info(`VRAM Available: ${(await this.vramAmount).toString()}mb`);
 	}
 
 	public async main() {
