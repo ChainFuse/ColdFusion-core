10c07d4533da8b8579fe0ed1ffba6068434c880b
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-08-13 00:37:03 -0700

    `join()` being weird

--- src/base.ts
+++ src/base.ts
@@ -4,7 +4,8 @@ import { format, join, parse } from 'node:path';
 
 export class BaseCore {
 	protected requestedOllamaVersion = getInput('ollama-version', { required: true, trimWhitespace: true });
-	protected ollamaPath = join(find('ollama', this.requestedOllamaVersion), '..', '..');
+	protected ollamaCachePath = find('ollama', this.requestedOllamaVersion);
+	protected ollamaPath = join(this.ollamaCachePath, '..', '..');
 	protected model: string = getInput('model', { required: true, trimWhitespace: true });
 	// Do format(parse()) for input validation
 	protected modelDir: string = format(parse(getInput('modelDir', { required: true, trimWhitespace: true })));
