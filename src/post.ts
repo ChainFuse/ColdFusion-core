1781783cf7be844fa409bda83a5ea4fccec21674
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-08-13 02:28:41 -0700

    debug

--- src/post.ts
+++ src/post.ts
@@ -1,7 +1,10 @@
+import { info } from '@actions/core';
 import { BaseCore } from './base.js';
 
 class PostCore extends BaseCore {
-	public async main() {}
+	public async main() {
+		info('Running post');
+	}
 }
 
 await new PostCore().main();
