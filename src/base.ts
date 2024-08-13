f4f0d9f53c85beabe880b1c5eb128ad74d4edb37
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-08-13 00:32:35 -0700

    Try `join` instead

--- src/base.ts
+++ src/base.ts
@@ -1,10 +1,10 @@
 import { getInput } from '@actions/core';
 import { find } from '@actions/tool-cache';
-import { format, parse, resolve } from 'node:path';
+import { format, join, parse } from 'node:path';
 
 export class BaseCore {
 	protected requestedOllamaVersion = getInput('ollama-version', { required: true, trimWhitespace: true });
-	protected ollamaPath = resolve(find('ollama', this.requestedOllamaVersion), '..', '..');
+	protected ollamaPath = join(find('ollama', this.requestedOllamaVersion), '..', '..');
 	protected model: string = getInput('model', { required: true, trimWhitespace: true });
 	// Do format(parse()) for input validation
 	protected modelDir: string = format(parse(getInput('modelDir', { required: true, trimWhitespace: true })));
