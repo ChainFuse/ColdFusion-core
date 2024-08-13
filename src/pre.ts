8dc3e855496fc88ab4b27c94088c47b15a1b0c89
DemosJarco <1209494+demosjarco@users.noreply.github.com>
2024-08-12 21:57:44 -0700

    debug

--- src/pre.ts
+++ src/pre.ts
@@ -100,6 +100,8 @@ export class PreCore extends BaseCore {
 
 					const hashType = /^sha\d{3}/i.exec(hashAsset!.name.toLowerCase())![0];
 					return FileHasher.hashFile(ollamaToolGuid, hashType).then((computedHash) => {
+						console.info('expectedHash', expectedHash, 'computedHash', computedHash);
+
 						if (timingSafeEqual(Buffer.from(computedHash, 'hex'), Buffer.from(expectedHash!, 'hex'))) {
 							return cacheFile(ollamaToolGuid, '/usr/local/bin/ollama', 'ollama', coerce(release!.tag_name)!.toString());
 						} else {
